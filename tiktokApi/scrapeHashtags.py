0d16aecdf3f256d6bb9c05a41174553cbe4f93fa
Liad Kehila <Liad3131@gmail.com>
2021-11-28 19:03:08 +0200

    sending each user seperately now

--- tiktokApi/scrapeHashtags.py
+++ tiktokApi/scrapeHashtags.py
@@ -1,27 +1,35 @@
-import scraper
-numPosts=10
+from scraper import scraper
+import requests
+import json
+from time import sleep
+numPosts=300
 postsPerUser=5
+since=0
+before=0
 hashtags=['أريحا']
-output=scraper.scrap_hashtags(hashtags,numPosts)
+output=scraper.scrap_hashtags(hashtags,numPosts,since,before)
 usersWithLocation=[user for user in output if user["governorate"]]
 # scrap some unnecessary info but do it in one command
-excessVideos=scraper.scrap_users([user["username"] for user in usersWithLocation],numPosts-1)
+# excessVideos=scraper.scrap_users([user["username"] for user in usersWithLocation],postsPerUser-1,since,before)
 for user in usersWithLocation:
+    res=[]
     videos=[]
     numVideosNeeded=postsPerUser-len(videos)
-    scrapedVideos=[scrapped["posts"] for scrapped in excessVideos if user["id"]==scrapped["id"]][0]
-    for video in user["posts"]+scrapedVideos[:numVideosNeeded]:
-        videos+={"Vid":video["id"],
-                "text":video["name"],
+    # scrapedVideos=[scrapped["posts"] for scrapped in excessVideos if user["id"]==scrapped["id"]][0]
+    for video in user["posts"]:
+        videos.append({"Vid":video["id"],
+                "text":video["description"],
                 "hashtags": video["hashtags"],
                 "musicId": video["music"]["id"],
-                "musicUrl": video["music"]["url"],
-                }
-    res={
+                "musicUrl": video["music"]["url"]
+                })
+    res.append({
         "id": user["id"],
         "governorate": user["governorate"],
-        "videos": [video["id"] for video in user["posts"]],
+        "videos": videos,
         "bio": user["bio"],
-        "userStats": user["stats"],
-    }
-    
+        "userStats": user["stats"]
+    })
+    x={"users":res}
+    headers = {'Content-Type': 'application/json', 'Accept':'application/json'}
+    requests.post("http://localhost:8001/api/database/postNewUsers",data=json.dumps(x), headers=headers)
