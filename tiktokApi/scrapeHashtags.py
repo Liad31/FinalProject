a7e6d5009ee68fe63cd51d118a13eab605d19329
Liad Kehila <Liad3131@gmail.com>
2021-12-07 00:05:18 +0200

    updated scraping script

--- tiktokApi/scrapeHashtags.py
+++ tiktokApi/scrapeHashtags.py
@@ -2,27 +2,32 @@ from scraper import scraper
 import requests
 import json
 from time import sleep
-numPosts=300
-postsPerUser=5
-since=0
-before=0
-hashtags=['أريحا']
-output=scraper.scrap_hashtags(hashtags,numPosts,since,before)
-usersWithLocation=[user for user in output if user["governorate"]]
+numPosts = 1000
+postsPerUser = 5
+since = 0
+before = 0
+with open("hashtags.txt", "r") as file:
+    hashtags = file.read().split(',')
+
+
+# hashtags= ["food","arab"]
+output = scraper.scrap_hashtags(hashtags, numPosts, since, before)
+usersWithLocation = [user for user in output if user["governorate"]]
+# usersWithLocation=output
 # scrap some unnecessary info but do it in one command
 # excessVideos=scraper.scrap_users([user["username"] for user in usersWithLocation],postsPerUser-1,since,before)
 for user in usersWithLocation:
-    res=[]
-    videos=[]
-    numVideosNeeded=postsPerUser-len(videos)
+    res = []
+    videos = []
+    # numVideosNeeded=postsPerUser-len(videos)
     # scrapedVideos=[scrapped["posts"] for scrapped in excessVideos if user["id"]==scrapped["id"]][0]
     for video in user["posts"]:
-        videos.append({"Vid":video["id"],
-                "text":video["description"],
-                "hashtags": video["hashtags"],
-                "musicId": video["music"]["id"],
-                "musicUrl": video["music"]["url"]
-                })
+        videos.append({"Vid": video["id"],
+                       "text": video["description"],
+                       "hashtags": video["hashtags"],
+                       "musicId": video["music"]["id"],
+                       "musicUrl": video["music"]["url"]
+                       })
     res.append({
         "id": user["id"],
         "governorate": user["governorate"],
@@ -30,6 +35,9 @@ for user in usersWithLocation:
         "bio": user["bio"],
         "userStats": user["stats"]
     })
-    x={"users":res}
-    headers = {'Content-Type': 'application/json', 'Accept':'application/json'}
-    requests.post("http://localhost:8001/api/database/postNewUsers",data=json.dumps(x), headers=headers)
+    x = {"users": res}
+    headers = {'Content-Type': 'application/json',
+               'Accept': 'application/json'}
+    requests.post("http://localhost:8001/api/database/postNewUsers",
+                  data=json.dumps(x), headers=headers)
+    print("hi")
