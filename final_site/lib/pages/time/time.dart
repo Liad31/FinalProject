da74b1c8c5c87d957f2daf2e9a0f432cf2dbba1f
CatGuy1300 <guyv22@gmail.com>
2022-05-16 16:29:11 +0300

    try

--- final_site/lib/pages/time/time.dart
+++ final_site/lib/pages/time/time.dart
@@ -167,7 +167,12 @@ class TimePage extends StatelessWidget {
 
 Future<List<DailyNationalisticData>> getChartData() async {
   var url = Uri.parse('http://104.154.93.111:8080/avgScoreOverTime');
-  var response = await http.get(url);
+  var response = await http.get(
+    url,
+    headers: <String, String>{
+      'x-requested-with': 'f',
+    },
+  );
   var list = jsonDecode(response.body);
   // print('Response status: $list');
   if (response.statusCode != 200) {
